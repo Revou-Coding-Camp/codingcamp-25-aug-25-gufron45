9ffc3bb09298e58bd7a32d0dd538ac760e332687
Dede <dedegufron@gmail.com>
2025-08-29 19:52:51 +0700

    Add initial HTML structure, CSS styles, and JavaScript functions for the portfolio website

---
+++ js/script.js
@@ -0,0 +1,38 @@
+menyapa ()
+
+function menyapa() {
+    let nama = prompt("Tolong Masukkan Nama Anda : ");
+   document.getElementById("nama").innerHTML = nama;
+}
+
+function validasiform() {
+    let nama = document.getElementById("nama-pengunjung").value;
+    let nomor = document.getElementById("nomor-pengunjung").value;
+    let email = document.getElementById("email-pengunjung").value;
+    let pesan = document.getElementById("pesan").value;
+
+    if (nama === "") {
+        alert("Nama kamu siapaaa???");
+        return;
+    }
+    if (nomor === "") {
+        alert("Aku boleh tau nomormu gak?");
+        return;
+    }
+    if (email === "") {
+        alert("Isi emailnya dongggg");
+        return;
+    }
+    if (pesan === "") {
+        alert("Kok gak ngasih pesan? kamu marah yaa?");
+        return;
+    }
+
+    // Tampilkan hasil ke kotak samping form
+    document.getElementById("hasil").innerHTML = `
+        <b>Nama:</b> ${nama}<br>
+        <b>Nomor HP:</b> ${nomor}<br>
+        <b>Email:</b> ${email}<br>
+        <b>Pesan:</b> ${pesan}
+    `;
+}
